d6db83489e9c419013939c6670696c9b8280860e
Giulia-Ciampelli <g.ciampelli2505@gmail.com>
2024-12-17 18:10:05 +0100

    docs: user details card map update

--- src/components/UserDetailsCard.jsx
+++ src/components/UserDetailsCard.jsx
@@ -1,3 +1,5 @@
+import { useParams } from 'react-router-dom';
+
 // Link su nome, mostrare a schermo mail, numero di telefono e codice fiscale
 import TravelCustomers from '../data/TravelCostumers.js';
 
@@ -6,22 +8,23 @@ import TravelCustomers from '../data/TravelCostumers.js';
 // aspettiamo prop del singolo customer da task 2
 
 export default function UserDetailsCard() {
+    const { id } = useParams();
+
     return (
         <div>
 
             {/* correggi il map */}
-            {/* test */}
             {TravelCustomers.map(customer => {
                 <ul>
-                    <li key={index}>
+                    <li key={customer.id}>
                         <p>
-                            email
+                            Mail: {customer.email}
                         </p>
                         <p>
-                            telefono
+                            Numero di telefono: {customer.numero}
                         </p>
                         <p>
-                            codice fiscale
+                            Codice fiscale: {customer.codice_fiscale}
                         </p>
                     </li>
                 </ul>
