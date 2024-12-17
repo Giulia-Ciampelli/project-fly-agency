4b3e0b4574fa5f48f2e9bf6007688c72f8a466c6
dariopalmesi <dariopalmesi88@gmail.com>
2024-12-17 17:25:59 +0100

    add comment passengers

--- src/pages/Passengers.jsx
+++ src/pages/Passengers.jsx
@@ -7,6 +7,7 @@ export default function getData() {
                 <div className="row">
                     {Custumers.map(custumer => (
                         <ul className="list-unstyled card p-3 mb-3 text-center" key={custumer.id}>
+                            {/* cambiare la key da id a citta_del_viaggio */}
                             <li>
                                 <div>{custumer.nome} {custumer.cognome} </div>
 
