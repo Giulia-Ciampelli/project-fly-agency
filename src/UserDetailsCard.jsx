a0aa994f8672a3aaa896173cd3b191e80aa7416d
Giulia-Ciampelli <g.ciampelli2505@gmail.com>
2024-12-17 15:35:41 +0100

    docs: card start

---
+++ src/UserDetailsCard.jsx
@@ -0,0 +1,10 @@
+// Link su nome, mostrare a schermo mail, numero di telefono e codice fiscale
+
+// componenti principali: aspettare team task 1
+
+export default function UserDetailsCard() {
+    return (
+        <div>
+        </div>
+    )
+}
