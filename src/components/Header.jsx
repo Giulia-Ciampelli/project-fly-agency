08da3b17957f7b1f46c0ea1ea21bb0f32e8d705b
LuigiSerra91 <luigi_serra91@hotmail.it>
2024-12-17 16:23:42 +0100

    add_bg-header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,10 @@
 export default function Header() {
     return (
-        <h1>Header</h1>
+        <>
+            <header className="d-flex align-items-center justify-content-between p-2 bg-primary mb-4">
+                <h1>Logo azienda</h1>
+                <h2>nome dell'azienda</h2>
+            </header>
+        </>
     )
 }
