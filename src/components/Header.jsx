0ec35e7d4bcb24cd392af0137138c1da4abf4ddb
LuigiSerra91 <luigi_serra91@hotmail.it>
2024-12-18 10:09:48 +0100

    add-link-in-the-logo

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,9 @@
+import { Link } from "react-router-dom"
 export default function Header() {
     return (
         <>
             <header className="d-flex align-items-center justify-content-between p-2 bg-primary mb-4">
-                <h1>Logo azienda</h1>
+                <Link to={"/"}><h1 className="text-black">Logo azienda</h1></Link>
                 <h2>nome dell'azienda</h2>
             </header>
         </>
--- src/pages/Passengers.jsx
+++ src/pages/Passengers.jsx
@@ -5,13 +5,13 @@ export default function getData() {
 
     const { citta_del_viaggio } = useParams()
     // la chiave non è unica, dobbiamo trovare un metodo diverso da useParams
-    console.log(citta_del_viaggio)
+    const filteredCustomers = Custumers.filter(custumer => custumer.citta_del_viaggio === citta_del_viaggio)
 
     return (
         <>
             <div className="container">
                 <div className="row">
-                    {Custumers.map(custumer => (
+                    {filteredCustomers.map(custumer => (
                         <ul className="list-unstyled card p-3 mb-3 text-center" key={custumer.citta_del_viaggio}>
                             {/* cambiare la key da id a citta_del_viaggio */}
                             <li>
