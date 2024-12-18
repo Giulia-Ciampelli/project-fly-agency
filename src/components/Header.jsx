b9d90fe679c8d8d78a7e3a2fbd231269eaa1b07e
LuigiSerra91 <luigi_serra91@hotmail.it>
2024-12-18 10:40:07 +0100

    remove-decoration-link

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ export default function Header() {
     return (
         <>
             <header className="d-flex align-items-center justify-content-between p-2 bg-primary mb-4">
-                <Link to={"/"}><h1 className="text-black">Logo azienda</h1></Link>
+                <Link style={{ textDecoration: 'none' }} to={"/"}><h1 className="text-black">Logo azienda</h1></Link>
                 <h2>nome dell'azienda</h2>
             </header>
         </>
