d1c487d7708bb3cbd226478af851fa0942460571
LuigiSerra91 <luigi_serra91@hotmail.it>
2024-12-18 15:30:01 +0100

    fix-footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,8 +2,9 @@ export default function Footer() {
     return (
 
         <>
-            <footer className="bg-success p-1">
-                <h2>Footer test</h2>
+            <footer className="bg-success p-1 d-flex justify-content-around align-items-center">
+                <h2>Copyright</h2>
+                <h2>Social</h2>
             </footer>
         </>
     )
